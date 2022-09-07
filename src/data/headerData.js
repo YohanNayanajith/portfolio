7a8b5030895d71c921546fe8d95951e459a4c6cd
YohanNayanajith <yohannayanajith40@gmail.com>
2022-09-07 18:26:48 +0530

    profile image changed

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Yohan Nayanajith',
     title: "Software Engineering Undergraduate",
     desciption:"The price of success is hard work, dedication to the job at hand, and the determination that whether we win or lose, we have applied the best of ourselves to the task at hand.",
-    image: 'https://res.cloudinary.com/midefulness/image/upload/v1662187729/My%20Photo/Yohan_Nayanajith_-_UOC_u4wqnf.png',
+    image: 'https://res.cloudinary.com/midefulness/image/upload/v1662555347/My%20Photo/yohan_new_mq84c2.jpg',
     resumePdf: resume
 }
